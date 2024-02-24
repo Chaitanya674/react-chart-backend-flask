dc0f03b02f376d07c6ca89030aa225f3627c5bb9
Chaitanya674 <chaitanyasharma674@gmail.com>
2024-02-24 12:55:10 +0530

    Backend files

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask ,jsonify
+from pymongo import MongoClient
+from flask_cors import CORS
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+
+app = Flask(__name__)
+CORS(app) 
+
+client = MongoClient(MONGO_URI)
+db = client["Database"]
+collection = db["Data"]
+
+
+@app.route("/")
+def get_data():
+    try:
+        data_from_mongo = list(collection.find({}))
+        for item in data_from_mongo:
+            item['_id'] = str(item['_id'])
+        json_data = jsonify({'data': data_from_mongo})
+        return json_data, 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Run the Flask app
+if __name__ == "__main__":
+    app.run(debug=True)
